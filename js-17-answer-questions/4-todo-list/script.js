d09dfd8da53ae8825f5e0fafeec487ce49ce632f
hadiseh-a <hadisea82@gmail.com>
2024-05-23 13:39:00 +0330

    17

--- js-17-answer-questions/4-todo-list/script.js
+++ js-17-answer-questions/4-todo-list/script.js
@@ -1,12 +1,40 @@
+const content = document.querySelector("#content");
+// const form = document.createElement("form");
+// content.append(form);
+// const label = document.createElement("label");
+// label.setAttribute("for", "todo");
+// label.innerText = "Add your work :";
+// form.append(label);
+// const input = document.createElement("input");
+// input.setAttribute("type", "text");
+// input.setAttribute("name", "todo");
+// form.append(input);
+// const button = document.createElement("button");
+// form.append(button);
+// button.innerHTML = "add";
+const ulist = document.createElement("ul");
+content.append(ulist);
 function todoList(todos) {
   // Write your code here...
+  for (const doing of todos) {
+    const li = document.createElement("li");
+    ulist.append(li);
+    li.innerText = doing.todo;
+    done(li);
+  }
+}
+function done(doing){
+  doing.addEventListener("click",()=>
+  {
+    doing.classList.toggle("lineThrough");
+  })
 }
 
 const todos = [
-  {todo: "wash the dishes"}, 
-  {todo: "walk the dog"}, 
-  {todo: "learn javascript"}, 
-  {todo: "go shopping"}
+  { todo: "wash the dishes" },
+  { todo: "walk the dog" },
+  { todo: "learn javascript" },
+  { todo: "go shopping" },
 ];
 
 todoList(todos);
