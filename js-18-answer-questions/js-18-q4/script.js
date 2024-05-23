d385f9b0d8f8e5451d4b285240def7da9919ea3e
hadiseh-a <hadisea82@gmail.com>
2024-05-23 18:22:53 +0330

    18

---
+++ js-18-answer-questions/js-18-q4/script.js
@@ -0,0 +1,37 @@
+// ? Display the following array on the page
+// todo-1: First, watch the video in the help folder.
+// todo-2: In the next step, review the HTML and CSS files.
+// todo-3: Iterate over the "data" array and create a card for each array index.
+// todo-4: Append the created card to the element with the class "cards".
+// todo-5: Each card should have a button; when clicked, the card title should be displayed on the element with the class "cardSelected".
+
+const data = [
+  {
+    id: 1,
+    title: "ravel Monkey Club #1",
+    src: "https://themesflat.co/html/axiesv/assets/images/box-item/card-item-9.jpg",
+    account: "Ralph Garraway",
+    accountImage: "https://themesflat.co/html/axiesv/assets/images/avatar/avt-12.jpg",
+  },
+  {
+    id: 2,
+    title: "ravel Monkey Club #2",
+    src: "https://themesflat.co/html/axiesv/assets/images/box-item/image-box-21.jpg",
+    account: "Ralph Garraway",
+    accountImage: "https://themesflat.co/html/axiesv/assets/images/avatar/avt-12.jpg",
+  },
+  {
+    id: 3,
+    title: "ravel Monkey Club #3",
+    src: "https://themesflat.co/html/axiesv/assets/images/box-item/image-box-10.jpg",
+    account: "Ralph Garraway",
+    accountImage: "https://themesflat.co/html/axiesv/assets/images/avatar/avt-12.jpg",
+  },
+  {
+    id: 4,
+    title: "ravel Monkey Club #4",
+    src: "https://themesflat.co/html/axiesv/assets/images/box-item/card-item8.jpg",
+    account: "Ralph Garraway",
+    accountImage: "https://themesflat.co/html/axiesv/assets/images/avatar/avt-12.jpg",
+  },
+];
