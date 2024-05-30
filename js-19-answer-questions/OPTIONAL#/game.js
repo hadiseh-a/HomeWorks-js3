f5829ab2bb9c0e2ae0d507648bb392bc00c254fa
hadiseh-a <hadisea82@gmail.com>
2024-05-30 15:33:57 +0330

    19&20&21&22

---
+++ js-19-answer-questions/OPTIONAL#/game.js
@@ -0,0 +1,10 @@
+/*
+  Build a guessing game:
+
+  - The system is picking a random number from 1 to 6.
+  - A user can enter a number. (You can use prompt or input or ...)
+  - If the user's number is equal to a random number, give the user 2 points.
+  - If the user's number is different than the random number by 1, give the user 1 point. Otherwise, give the user 0 points.
+  - The user can play the game as long as they want to.
+  - Note: you should use "Promise" to resolve this question. For example, you can create promise for check the input number. 
+*/
---
+++ js-21-answer-questions/make-website/make-website.js
@@ -0,0 +1,11 @@
+/*
+We want you to build a website. Until now you have learned all the basics needed for building a full and functional website via javascript. For this assignment you will have 2 weeks time.
+For this assignment you must consider these notes:
+Here is a repo full of APIs (https://github.com/public-apis/public-apis). This repo is a gathering of tremendous numbers of APIs around the internet. You are free to choose any of them (or you can use any other APIs).
+Your website should be a complete website. This means that your website should have at least 4-5 pages.
+Some ideas that you can use are the HomePage - AboutPage – Login/SignUp pages – single product page (if your website is commercial) – contact-us page – dashboard page – admin panel etc. this doesn’t mean that you should build all of these pages. They are just ideas, you can structure your website as you wish.
+Your website should use at least 1 APIs.
+You can use any CSS libraries you want (or you can use just pure CSS)
+This isn’t a team work. You should build your own website.
+Your login page should include validation for the user's credentials. If the entered credentials are correct, the user should be redirected to the home page. If the credentials are incorrect, an error message should be displayed to inform the user of their invalid credentials.
+*/
